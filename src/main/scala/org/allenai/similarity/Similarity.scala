e6dd151ff76d44cdfcec97d57d29d4fac7682583
Niranjan Balasubramanian <niranjan@cs.washington.edu>
2014-04-25 15:42:12 -0700

    Fix word overlap bug

--- src/main/scala/org/allenai/similarity/Similarity.scala
+++ src/main/scala/org/allenai/similarity/Similarity.scala
@@ -8,13 +8,25 @@ trait Similarity {
 
 }
 
+object WordOverlapTest extends App {
+  var x = "brown dog"
+  var y = "quick dog"
+  val overlap = new WordOverlap
+  println(s"Similarity(${x}, ${y})=${overlap.similarity(x, y)}")
+  println(s"Similarity(${x}, ${x})=${overlap.similarity(x, x)}")
+  println(s"Similarity(${y}, ${y})=${overlap.similarity(y, y)}")
+}
+
 class WordOverlap extends Similarity{
+
   override def similarity(x: String, y: String): Double = {
     def tokens(string:String) = string.split(" ").toSet
     val xtokens = tokens(x)
     val ytokens = tokens(y)
-    val intersection = xtokens.intersect(ytokens)
-    val union = xtokens.intersect(ytokens)
-    intersection.size.toDouble/union.size.toDouble
+    dice(xtokens, ytokens)
   }
+
+  def dice(xtokens: Set[String], ytokens: Set[String]): Double
+    = xtokens.intersect(ytokens).size.toDouble/xtokens.union(ytokens).size
+
 }
