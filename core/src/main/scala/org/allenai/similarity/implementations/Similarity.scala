18c52a23bbe07b5412397cacf19102441733fe40
Niranjan Balasubramanian <niranjan@cs.washington.edu>
2014-04-30 17:41:53 -0700

    Comment

--- core/src/main/scala/org/allenai/similarity/implementations/Similarity.scala
+++ core/src/main/scala/org/allenai/similarity/implementations/Similarity.scala
@@ -2,5 +2,9 @@ package org.allenai.similarity.implementations
 
 trait Similarity {
 
+  //Name of the similarity method.
+  def name: String
+
+  //Similarity between text and hypothesis strings.
   def similarity(text:String, hypothesis:String):Double
 }
--- core/src/main/scala/org/allenai/similarity/implementations/WordOverlap.scala
+++ core/src/main/scala/org/allenai/similarity/implementations/WordOverlap.scala
@@ -4,6 +4,8 @@ import org.allenai.similarity.Measures
 
 class WordOverlap extends Similarity{
 
+  override val name = "Bag-of-words"
+
   def tokens(string:String) = string.split(" ").toSet
   override def similarity(text: String, hypothesis: String): Double =
     Measures.jaccard(tokens(text), tokens(hypothesis))
